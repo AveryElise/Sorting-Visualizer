99549d881d7fad897b09d25f70cbcd3409031d29
AveryElise <avery.cecilia@gmail.com>
2020-05-03 12:37:30 -0700

    Clean up + Merge Sort

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 var numDataPoints = document.getElementById('slider').value;
 var datawidth = (canvas.width - points.length)/ numDataPoints;
-var ssdone;
-var isdone;
 
 //fill data on load
 points = getDataPoints(numDataPoints);
 draw(points);
 
-//for generator next functions
+//for generator functions
 var sort;
 
 //draw new canvas every time slider is updated
@@ -34,18 +32,16 @@ function getDataPoints(n){
 }
 
 
-function* bubblesort(arr=[]){
+function* bubbleSort(arr=[]){
 	do{
 		swapped = false;
 		let x = 0;
 		for (let i = 0; i < arr.length - x; i++){
 			if (arr[i]>arr[i+1]){
 				swapped=  true;
-				draw(arr, i, i+1);
-				yield;
 				swap(arr, i, i+1);
 				draw(arr, i, i+1);
-				yield; // pause here
+				yield;
 			}
 		} 
 		x++;
@@ -68,8 +64,6 @@ function* selectionSort(arr=[]){
 		let minIndx=i;
 		for (let j = i+1; j < arr.length; j++){
 			if (arr[minIndx] > arr[j]){
-				draw(arr, i, j);
-				yield;
 				swap(arr, j, i);
 				draw(arr, i, j);
 				yield;
@@ -87,8 +81,6 @@ function* insertionSort(arr=[]){
 		key=arr[i];
 		j = i-1;
 		while (j >=0 && key < arr[j]){
-			draw(points, i, j);
-			yield;
 			arr[j+1] = arr[j];
 			j--;
 			draw(points, i, j);
@@ -99,6 +91,52 @@ function* insertionSort(arr=[]){
 	isdone = true;
 	draw(points);
 	enableButtons();
+}
+
+
+function* mergeSort(arr=[]){
+	var n = arr.length;
+	var buffer = [];
+	for (var size=1; size < n; size = size*2){
+		for (var leftStart = 0; leftStart < n; leftStart = leftStart+(2*size)){
+			var left = leftStart;
+			var right = Math.min(left+size, n);
+			var leftLimit = right;
+			var rightLimit = Math.min(right + size, n);
+			var x=left;
+
+			while (left < leftLimit && right < rightLimit){
+				if (arr[left]<arr[right]){
+					buffer[x] = arr[left];
+					left++;
+					x++;
+				} else {
+					buffer[x] = arr[right];
+					right++;
+					x++;
+				}
+			}
+			while (left < leftLimit){
+				buffer[x] = arr[left];
+				left++;
+				x++;
+			}
+			while (right < rightLimit){
+				buffer[x] = arr[right];
+				right++;
+				x++;
+			}
+			//visualization section - copy the buffer to array
+			for (var j = leftStart; j < rightLimit; j++){
+				arr[j] = buffer[j];
+				draw(arr, j);
+				yield;
+			}
+		}
+	}
+	enableButtons();
+	draw(arr);
+	yield;
 }
 
 
@@ -143,45 +181,15 @@ function enableButtons(){
 	slider.disabled = false;
 }
 
-function bsAnimate(){
-	if (swapped == true){
-		requestAnimationFrame(bsAnimate);
+function animate(){
+	if (sort.next().done == false){
+		requestAnimationFrame(animate);
 	}
 	sort.next();
 }
 
-function bsStart(){
+function startSort(func){
 	disableButtons();
-	swapped = true;
-	sort = bubblesort(points);
-	bsAnimate();
-}
-
-function ssAnimate(){
-	if (ssdone == false){
-		requestAnimationFrame(ssAnimate);
-	}
-	sort.next();
-}
-
-function ssStart(){
-	disableButtons();
-	ssdone=false;
-	sort=selectionSort(points);
-	ssAnimate();
-}
-
-
-function isAnimate(){
-	if (isdone == false){
-		requestAnimationFrame(isAnimate);
-	}
-	sort.next();
-}
-
-function isStart(){
-	disableButtons();
-	isdone = false;
-	sort = insertionSort(points);
-	isAnimate();
+	sort = func(points);
+	animate();
 }
